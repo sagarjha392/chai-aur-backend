803f7c0aa42c933969cebf0d66eab6c66d479e3f
Sagar Kumar Jha <55592017+sagarjha392@users.noreply.github.com>
2024-02-10 12:59:41 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,7 +8,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from frontend
     const { username, email, fullName, password } = req.body
-    console.log(email);
+    // console.log(email);
     // validation not empty
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -16,7 +16,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
     // check if user already exists: username, email
-    const existingUser = User.findOne({
+    const existingUser = await User.findOne({
         $or: [{ username }, { email }]
     })
     if (existingUser) {
@@ -24,7 +24,12 @@ const registerUser = asyncHandler(async (req, res) => {
     }
     // check for images, check  for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -18,7 +18,8 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
 
         // file uploaded successfully 
-        console.log("file is uploaded on cloudinary", response.url);
+        // console.log("file is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
         return response
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove locally saved temporary file if upload operation changed
